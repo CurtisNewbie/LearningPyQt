50821b19f51d821bac1884269bf943f2fc9f85ff
yongj.zhuang <yongj.zhuang@outlook.com>
2020-12-13 15:41:55 +0800

    QLabel

---
+++ qlabel.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+
+# application
+app = QApplication([])
+
+# create label with text
+label = QLabel('Hello World')
+
+# show the label
+label.show()
+
+# startup app
+app.exec_()
+
